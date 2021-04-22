445233392f0fc040b63611a1f5e83b2534c50164
Potikorn <p.thammo@20scoops.net>
2021-04-22 18:02:32 +0700

    mock up send input view

--- App.tsx
+++ App.tsx
@@ -10,11 +10,13 @@
 
 import React, {useEffect, useState} from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   TouchableHighlight,
   useColorScheme,
   View,
@@ -35,9 +37,11 @@ const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
   const [wallet, SetWallet] = useState<Wallet | null>(null);
   const [revealToggleWallet, setToggleRevealWallet] = useState(false);
+  const [toggleAddress, setToggleAddress] = useState(false);
+  const [toggleSend, setToggleSend] = useState(false);
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: 'white',
   };
 
   useEffect(() => {
@@ -52,12 +56,78 @@ const App = () => {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <View style={styles.sectionContainer}>
-          <TouchableHighlight
-            disabled={wallet === null}
-            onPress={() => setToggleRevealWallet(true)}>
-            <Text style={styles.textButton}>Reveal phrase keyword</Text>
-          </TouchableHighlight>
-          {revealToggleWallet && <Text>{wallet?.mnemonic}</Text>}
+          {wallet === null ? (
+            <Text>Loading...</Text>
+          ) : (
+            <>
+              <TouchableHighlight
+                disabled={wallet === null}
+                onPress={() => setToggleRevealWallet(!revealToggleWallet)}>
+                <Text style={styles.textButton}>{`${
+                  revealToggleWallet ? 'Hidden' : 'Reveal'
+                } phrase keyword`}</Text>
+              </TouchableHighlight>
+              {revealToggleWallet && (
+                <View>
+                  <Text>{wallet?.mnemonic}</Text>
+                  {/* <Button
+                    onPress={() => setToggleAddress(true)}
+                    title="Get Address"
+                  />
+                  {toggleAddress && <Text>{wallet.address}</Text>} */}
+                </View>
+              )}
+              <View>
+                <View style={styles.buttonWrapper}>
+                  <View style={{flex: 1, paddingRight: 5}}>
+                    <Button
+                      onPress={() => {
+                        setToggleSend(!toggleSend);
+                        setToggleAddress(false);
+                      }}
+                      title="Send"
+                    />
+                  </View>
+                  <View style={{flex: 1, paddingLeft: 5}}>
+                    <Button
+                      onPress={() => {
+                        setToggleAddress(!toggleAddress);
+                        setToggleSend(false);
+                      }}
+                      title="Receive"
+                    />
+                  </View>
+                </View>
+                {toggleAddress && (
+                  <Text selectable={true}>{wallet.address}</Text>
+                )}
+                {toggleSend && (
+                  <View>
+                    <TextInput
+                      placeholder="To address"
+                      style={styles.textInput}
+                    />
+                    <TextInput
+                      keyboardType="numeric"
+                      placeholder="Gas price"
+                      style={{...styles.textInput, marginTop: 10}}
+                    />
+                    <View style={{marginTop: 16}}>
+                      <Button
+                        onPress={() => {
+                          wallet.sendTransaction({
+                            from: wallet.address,
+                            to: 'adf', //get from text input
+                          });
+                        }}
+                        title="Confirm send"
+                      />
+                    </View>
+                  </View>
+                )}
+              </View>
+            </>
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -83,6 +153,16 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  buttonWrapper: {
+    flexDirection: 'row',
+    paddingVertical: 10,
+  },
+  textInput: {
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: 'gray',
   },
 });
 
